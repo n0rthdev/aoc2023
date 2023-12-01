ed79867fc4c6377b86a3e9d48923e3ad67d8caec
Martin Weber <martin.weber@teamecho.at>
2023-12-01 20:02:08 +0100

    improve day 1 part 2

--- src/main/kotlin/Day01Part2.kt
+++ src/main/kotlin/Day01Part2.kt
@@ -10,24 +10,39 @@ class Day01Part2 : LinesPuzzle() {
 
     override fun solve(lines: List<String>): String {
 
+        return lines.sumOf { line ->
+            val newline = line.replace("one","one1one").
+            replace("two","two2two").
+            replace("three","three3three").
+            replace("four","four4four").
+            replace("five","five5five").
+            replace("six","six6six").
+            replace("seven","seven7seven").
+            replace("eight","eight8eight").
+            replace("nine","nine9nine")
+            "${newline.first { it.isDigit() }}${newline.last { it.isDigit() }}".toLong()
+        }.toString()
+    }
+
+    fun solveLong(lines: List<String>): String {
+
         val translationMap = mapOf(
-            "two" to "2",
             "one" to "1",
-            "eight" to "8",
+            "two" to "2",
             "three" to "3",
             "four" to "4",
             "five" to "5",
             "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
             "nine" to "9",
-            "seven" to "7"
         )
-
 
         return lines.sumOf { line ->
 
             val numbers = mutableListOf<Int>()
 
-            for (i in range(0, line.length)) {
+            for (i in line.indices) {
                 if (line[i].isDigit()) {
                     numbers.add(line[i].code - '0'.code)
                 } else {
