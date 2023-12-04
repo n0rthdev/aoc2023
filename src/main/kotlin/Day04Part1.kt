f8d637293ce04d80951bea9bb477fc2035010b66
Martin Weber <martin.weber@teamecho.at>
2023-12-04 06:37:33 +0100

    day 4 clean up

--- src/main/kotlin/Day04Part1.kt
+++ src/main/kotlin/Day04Part1.kt
@@ -4,14 +4,17 @@ class Day04Part1 : LinesPuzzle() {
     override fun solve(lines: List<String>): String {
         val res = lines.map { line ->
             val nl = line.split(":")[1]
-            var (win, my) = nl.split(" | ")
+            val (win, my) = nl.split(" | ")
+                .map {
+                    it.split(" ")
+                        .filter { it.trim().isNotBlank() }
+                        .map { it.trim().toLong() }
+                        .toSet()
+                }
 
-            val winNr = win.trim().split(" ").filter { it.trim().isNotBlank() }.map { it.trim().toLong() }.toSet()
-            val myNr = my.trim().split(" ").filter { it.trim().isNotBlank() }.map { it.trim().toLong() }.toSet()
-
-            val correctNumbers = myNr.intersect(winNr).size
+            val correctNumbers = my.intersect(win).size
             if (correctNumbers != 0) {
-                Math.pow(2.0, (myNr.intersect(winNr).size * 1.0) - 1.0).toLong()
+                Math.pow(2.0, correctNumbers - 1.0).toLong()
             } else {
                 0L
             }
--- src/main/kotlin/Day04Part2.kt
+++ src/main/kotlin/Day04Part2.kt
@@ -4,20 +4,22 @@ class Day04Part2 : LinesPuzzle() {
     override fun solve(lines: List<String>): String {
         val cards = lines.map { line ->
             val (cardName,nl) = line.split(":")
-            var (win, my) = nl.split(" | ")
-
-            val winNr = win.trim().split(" ").filter { it.trim().isNotBlank() }.map { it.trim().toLong() }.toSet()
-            val myNr = my.trim().split(" ").filter { it.trim().isNotBlank() }.map { it.trim().toLong() }.toSet()
+            val (win, my) = nl.split(" | ")
+                .map {
+                    it.split(" ")
+                        .filter { it.trim().isNotBlank() }
+                        .map { it.trim().toLong() }
+                        .toSet()
+                }
 
             Card(cardName.substringAfter("Card").trim().toLong(),
-                winNr,
-                myNr)
+                win,
+                my)
         }.associateBy { it.id }
 
         var myCards = cards.values
         var count = 0L
-        while (myCards.isNotEmpty())
-        {
+        while (myCards.isNotEmpty()) {
             val newCards = mutableListOf<Card>()
             for(mc in myCards) {
                 count++
