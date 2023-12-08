419332892286d22ec85c9f7908045048e44981f1
Martin Weber <martin.weber@teamecho.at>
2023-12-08 18:08:40 +0100

    generic edge

--- src/main/kotlin/utils/AStar.kt
+++ src/main/kotlin/utils/AStar.kt
@@ -2,9 +2,9 @@ package utils
 
 import java.util.*
 
-object AStar {
-    fun findPath(begin: Vertex): VertexWithCost {
-        val priorityQueue = PriorityQueue<VertexWithCost>()
+class AStar<E: AStar.Edge> {
+    fun findPath(begin: Vertex<E>): VertexWithCost<E> {
+        val priorityQueue = PriorityQueue<VertexWithCost<E>>()
 
         var currentVertex = VertexWithCost.forStart(begin)
         while (priorityQueue.isNotEmpty()) {
@@ -20,9 +20,9 @@ object AStar {
         }
         throw IllegalArgumentException("No Path")
     }
-    fun traversePath(goal: VertexWithCost) :AStarResul{
-        var current :VertexWithCost? = goal
-        val edgeList = mutableListOf<Edge>()
+    fun traversePath(goal: VertexWithCost<E>) :AStarResul<E>{
+        var current :VertexWithCost<E>? = goal
+        val edgeList = mutableListOf<E>()
         while(current?.previous != null){
             val edge = current.edge
             if(edge != null){
@@ -32,42 +32,43 @@ object AStar {
         }
         return AStarResul(goal, edgeList.reversed())
     }
-}
 
-class VertexWithCost private constructor(val vertex: Vertex, val cost: Long, val previous: VertexWithCost?, val edge: Edge?) :
-    Comparable<VertexWithCost> {
-    val estimatedTotalCost = cost + vertex.heuristic()
-    val isGoal get() = vertex.isGoal()
+    class VertexWithCost<E: Edge> private constructor(val vertex: Vertex<E>, val cost: Long, val previous: VertexWithCost<E>?, val edge: E?) :
+        Comparable<VertexWithCost<E>> {
+        val estimatedTotalCost = cost + vertex.heuristic()
+        val isGoal get() = vertex.isGoal()
 
-    override fun compareTo(other: VertexWithCost): Int {
-        return estimatedTotalCost.compareTo(other.estimatedTotalCost)
-    }
+        override fun compareTo(other: VertexWithCost<E>): Int {
+            return estimatedTotalCost.compareTo(other.estimatedTotalCost)
+        }
 
-    fun getNextVertices(): List<VertexWithCost> {
-        return vertex.getEdges().map {
-            traverse(it)
+        fun getNextVertices(): List<VertexWithCost<E>> {
+            return vertex.getEdges().map {
+                traverse(it)
+            }
+        }
+
+        fun traverse(edge: E): VertexWithCost<E> {
+            return forEdge(this.vertex.applyEdge(edge), cost + edge.cost, this, edge)
+        }
+
+        companion object {
+            fun <E: Edge>forStart(vertex: Vertex<E>) = VertexWithCost<E>(vertex, 0L, null, null)
+            fun <E: Edge>forEdge(vertex: Vertex<E>, cost: Long, previous: VertexWithCost<E>, edge: E) = VertexWithCost(vertex, cost, previous, edge)
         }
     }
 
-    fun traverse(edge: Edge): VertexWithCost {
-        return forEdge(this.vertex.applyEdge(edge), cost + edge.cost, this, edge)
+    data class AStarResul<E: Edge>(val goal: VertexWithCost<E>, val path: List<E>)
+
+    interface Vertex<E: Edge> {
+        fun heuristic(): Long
+        fun isGoal(): Boolean
+        fun applyEdge(edge: E): Vertex<E>
+        fun getEdges(): List<E>
     }
 
-    companion object {
-        fun forStart(vertex: Vertex) = VertexWithCost(vertex, 0L, null, null)
-        fun forEdge(vertex: Vertex, cost: Long, previous: VertexWithCost, edge: Edge) = VertexWithCost(vertex, cost, previous, edge)
+    interface Edge {
+        val cost: Long
     }
 }
 
-data class AStarResul(val goal: VertexWithCost, val path: List<Edge>)
-
-interface Vertex {
-    fun heuristic(): Long
-    fun isGoal(): Boolean
-    fun applyEdge(edge: Edge): Vertex
-    fun getEdges(): List<Edge>
-}
-
-interface Edge {
-    val cost: Long
-}
